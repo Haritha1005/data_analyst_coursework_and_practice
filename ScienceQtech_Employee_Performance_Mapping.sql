fb4f21ef006f3b167dd9980ac1c27f210ab3aa59
Haritha <93762176+Haritha1005@users.noreply.github.com>
2024-05-16 10:23:03 +0530

    ScienceQtech_Employee_Performance_Mapping.sql

--- ScienceQtech_Employee_Performance_Mapping.sql
+++ ScienceQtech_Employee_Performance_Mapping.sql
@@ -1,15 +1,30 @@
--- 1. Create database and import data
+### 1. Create database and import data 
+
 CREATE DATABASE employee;
 
--- Import CSV data into tables
-LOAD DATA INFILE 'data_science_team.csv' INTO TABLE employee.data_science_team
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+### Import CSV data into tables
 
-LOAD DATA INFILE 'proj_table.csv' INTO TABLE employee.proj_table
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+Created a database named employee and imported data_science_team.csv, proj_table.csv, and emp_record_table.csv into the employee database in MySQL Workbench
 
-LOAD DATA INFILE 'emp_record_table.csv' INTO TABLE employee.emp_record_table
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+### 2. Create an ER diagram for the given employee database.
+
+![ER Diagram]("C:\Data Analytics - Master course Simplilearn\Course 2 - SQL\ER Diagram_EmployeeDB.png")
+
+### 3. Fetch employee details and department
+
+SELECT EMP_ID, FIRST_NAME, LAST_NAME, GENDER, DEPT 
+FROM employee.emp_record_table;
+
+### 4. Fetch employee details based on rating
+
+SELECT EMP_ID, FIRST_NAME, LAST_NAME, GENDER, DEPT, EMP_RATING
+FROM employee.emp_record_table
+WHERE EMP_RATING < 2;
+
+SELECT EMP_ID, FIRST_NAME, LAST_NAME, GENDER, DEPT, EMP_RATING
+FROM employee.emp_record_table
+WHERE EMP_RATING > 4;
+
+SELECT EMP_ID, FIRST_NAME, LAST_NAME, GENDER, DEPT, EMP_RATING
+FROM employee.emp_record_table
+WHERE EMP_RATING BETWEEN 2 AND 4;
